1c37c96db45ed3fc5eb3d617ff21e268f79e3004
Dmytro Bartieniev <kuhelbeher@gmail.com>
2019-12-27 16:02:28 +0200

    Added signup route

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,36 @@
+import { Repository, EntityRepository } from 'typeorm';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp({ username, password }: AuthCredentialsDto): Promise<void> {
+    const user = new User();
+
+    const salt = await bcrypt.genSalt();
+
+    user.username = username;
+    user.salt = salt;
+    user.password = await this.hashPassword(password, salt);
+
+    try {
+      await user.save();
+    } catch (error) {
+      // duplicate username
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
+}
