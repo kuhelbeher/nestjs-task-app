146604acbf77db9e9620ce9097baa717ca0e44f7
Dmytro Bartieniev <kuhelbeher@gmail.com>
2019-12-28 15:17:37 +0200

    Added config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,6 +2,8 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as config from 'config';
+
 import { JwtPayload } from './jwt-payload.interface';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
@@ -13,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'somesecret',
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
     });
   }
 
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,25 @@
 /* eslint-disable prefer-template */
 import * as path from 'path';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const {
+  type,
+  port,
+  database,
+  host,
+  username,
+  password,
+  synchronize,
+} = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'password',
-  database: 'taskmanagement',
+  type,
+  host: process.env.RDS_HOSTNAME || host,
+  port: process.env.RDS_PORT || port,
+  username: process.env.RDS_USERNAME || username,
+  password: process.env.RDS_PASSWORD || password,
+  database: process.env.RDS_DB_NAME || database,
   entities: [path.join(__dirname, '../**/*.entity.{ts,js}')],
-  synchronize: true,
+  synchronize,
 };
